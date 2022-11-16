a469deb9e337b87b5396854c4261124476340514
Alprince Nacion <nacionalprince@gmail.com>
2022-11-16 22:01:46 +0800

    details Successfully Created and the variable  from index has been moved to a new file, named products

---
+++ products.php
@@ -0,0 +1,60 @@
+<?php
+	$arrProducts = array(
+		array(
+			'name' 		  => "Sage Green Shirt",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "550",
+			'photo1'	  => "product1A.jpg",
+			'photo2'	  => "product1B.jpg",
+		),
+		array(
+			'name' 		  => "Gray Shirt",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "550",
+			'photo1'	  => "product2A.jpg",
+			'photo2'	  => "product2B.jpg",
+		),
+		array(
+			'name' 		  => "White Blazer",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "750",
+			'photo1'	  => "product3A.jpg",
+			'photo2'	  => "product3B.jpg",
+		),
+		array(
+			'name' 		  => "Dark Blue Polo Shirt",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "600",
+			'photo1'	  => "product4A.jpg",
+			'photo2'	  => "product4B.jpg",
+		),
+		array(
+			'name' 		  => "Dark Blue Long Sleeves",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "800",
+			'photo1'	  => "product5A.jpg",
+			'photo2'	  => "product5B.jpg",
+		),
+		array(
+			'name' 		  => "White Long Sleeves",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "800",
+			'photo1'	  => "product6A.jpg",
+			'photo2'	  => "product6B.jpg",
+		),
+		array(
+			'name' 		  => "Dark Blue Blazer",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "750",
+			'photo1'	  => "product7A.jpg",
+			'photo2'	  => "product7B.jpg",
+		),
+		array(
+			'name' 		  => "Floral Polo",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+			'price'		  => "650",
+			'photo1'	  => "product8A.jpg",
+			'photo2'	  => "product8B.jpg",
+		),																			
+	);
+  ?>
